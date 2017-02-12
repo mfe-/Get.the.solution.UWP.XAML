27501af2cad007f9aaaafdd9f93cea8c2863e4a2
mfe <martin.fetzel@get-the-solution.net>
2017-02-12 13:37:48 +0100

    Added DefaultMeniItemTemplate for the uwp toolkit

--- Core/OpenMenuFlyoutAction.cs
+++ Core/OpenMenuFlyoutAction.cs
@@ -4,6 +4,16 @@ using Windows.UI.Xaml.Controls.Primitives;
 
 namespace Get.the.solution.UWP.XAML
 {
+    /// <summary>
+    /// Shows a <see cref="FlyoutBase"/>
+    /// <code>
+    /// <Interactivity:Interaction.Behaviors>
+    ///     <Core:EventTriggerBehavior EventName="RightTapped">
+    ///         <common:OpenMenuFlyoutAction />
+    ///     </Core:EventTriggerBehavior>
+    ///  </Interactivity:Interaction.Behaviors>
+    /// </code>
+    /// </summary>
     public class OpenMenuFlyoutAction : DependencyObject, IAction
     {
         public object Execute(object sender, object parameter)
@@ -12,7 +22,7 @@ namespace Get.the.solution.UWP.XAML
             FlyoutBase flyoutBase = FlyoutBase.GetAttachedFlyout(senderElement);
             if(flyoutBase!=null)
             {
-                flyoutBase.ShowAt(senderElement);
+               flyoutBase.ShowAt(senderElement);
             }
 
             return null;
