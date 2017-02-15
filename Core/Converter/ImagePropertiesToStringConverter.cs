ec5d0144e44b3f75a4d1afee9a1981f9f8c65b43
mfe <martin.fetzel@get-the-solution.net>
2017-02-15 13:03:55 +0100

    Added DeviceTypeHelper and ImagePropertiesToStringConverter

---
+++ Core/Converter/ImagePropertiesToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.FileProperties;
+using Windows.UI.Xaml.Data;
+
+namespace Get.the.solution.UWP.XAML.Converter
+{
+    public class ImagePropertiesToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return String.Empty;
+
+            ImageProperties ImageProperties = value as ImageProperties;
+            if (ImageProperties == null) return String.Empty;
+
+            return $"{ImageProperties.Width}x{ImageProperties.Height}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/DeviceTypeHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+
+namespace Get.the.solution.UWP.XAML
+{
+    /// <summary>
+    /// https://gist.github.com/wagonli/40d8a31bd0d6f0dd7a5d
+    /// </summary>
+    public static class DeviceTypeHelper
+    {
+        public static DeviceFormFactorType GetDeviceFormFactorType()
+        {
+            switch (AnalyticsInfo.VersionInfo.DeviceFamily)
+            {
+                case "Windows.Mobile":
+                    return DeviceFormFactorType.Phone;
+                case "Windows.Desktop":
+                    return UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Mouse
+                        ? DeviceFormFactorType.Desktop
+                        : DeviceFormFactorType.Tablet;
+                case "Windows.Universal":
+                    return DeviceFormFactorType.IoT;
+                case "Windows.Team":
+                    return DeviceFormFactorType.SurfaceHub;
+                default:
+                    return DeviceFormFactorType.Other;
+            }
+        }
+    }
+
+    public enum DeviceFormFactorType
+    {
+        Phone,
+        Desktop,
+        Tablet,
+        IoT,
+        SurfaceHub,
+        Other
+    }
+}
