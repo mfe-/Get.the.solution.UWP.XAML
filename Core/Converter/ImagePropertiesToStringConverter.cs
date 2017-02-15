777b8eff98d7a0115fd24b894c678df2263dcc47
mfe <martin.fetzel@get-the-solution.net>
2017-02-15 14:00:56 +0100

    ImagePropertiesToStringConverter

--- Core/Converter/ImagePropertiesToStringConverter.cs
+++ Core/Converter/ImagePropertiesToStringConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.Storage.FileProperties;
 using Windows.UI.Xaml.Data;
 
@@ -12,12 +13,29 @@ namespace Get.the.solution.UWP.XAML.Converter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
+            ImageProperties ImageProperties = null;
             if (value == null) return String.Empty;
+            if (value.GetType().Equals(typeof(StorageFile)))
+            {
+                StorageFile StorageFile = value as StorageFile;
+                ImageProperties = StorageFile.Properties.GetImagePropertiesAsync().GetAwaiter().GetResult();
+            }
+            else if (value.GetType().Equals(typeof(StorageFile)))
+            {
+                ImageProperties = value as ImageProperties;
+            }
 
-            ImageProperties ImageProperties = value as ImageProperties;
-            if (ImageProperties == null) return String.Empty;
+            if (ImageProperties != null)
+            {
+                if (ImageProperties == null) return String.Empty;
 
-            return $"{ImageProperties.Width}x{ImageProperties.Height}";
+                return $"{ImageProperties.Width}x{ImageProperties.Height}";
+            }
+            else
+            {
+                throw new ArgumentException($"Image StorageFile expected. Got {value.GetType()}");
+            }
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
--- README.md
+++ README.md
@@ -5,3 +5,11 @@ UWP XAML Helper Stuff
 Default XAML Style Dictionary for
 http://stackoverflow.com/questions/37336314/uwp-xaml-how-to-inherit-from-the-default-styles-using-basedon
 http://stackoverflow.com/questions/38825696/style-inheritance-how-to-refine-a-custom-default-style?noredirect=1&lq=1 
+
+
+Check for Platform/Device
+
+if (DeviceTypeHelper.GetDeviceFormFactorType() != DeviceFormFactorType.Desktop)
+{
+	...
+}
