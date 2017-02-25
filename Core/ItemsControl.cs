70f5bdcdac45e4777889a0d98e2b088d558fadcc
mfe <martin.fetzel@get-the-solution.net>
2017-02-25 11:54:56 +0100

    ItemsControl obsolet

--- Core/ItemsControl.cs
+++ Core/ItemsControl.cs
@@ -9,42 +9,42 @@ using Windows.UI.Xaml.Controls;
 
 namespace Get.the.solution.UWP.XAML
 {
-    public static class ItemClickCommand
-    {
-        public static readonly DependencyProperty CommandProperty =
-            DependencyProperty.RegisterAttached("Command", typeof(ICommand),
-            typeof(ItemClickCommand), new PropertyMetadata(null, OnCommandPropertyChanged));
+    //public static class ItemClickCommand
+    //{
+    //    public static readonly DependencyProperty CommandProperty =
+    //        DependencyProperty.RegisterAttached("Command", typeof(ICommand),
+    //        typeof(ItemClickCommand), new PropertyMetadata(null, OnCommandPropertyChanged));
 
-        public static void SetCommand(DependencyObject d, ICommand value)
-        {
-            d.SetValue(CommandProperty, value);
-        }
+    //    public static void SetCommand(DependencyObject d, ICommand value)
+    //    {
+    //        d.SetValue(CommandProperty, value);
+    //    }
 
-        public static ICommand GetCommand(DependencyObject d)
-        {
-            return (ICommand)d.GetValue(CommandProperty);
-        }
+    //    public static ICommand GetCommand(DependencyObject d)
+    //    {
+    //        return (ICommand)d.GetValue(CommandProperty);
+    //    }
 
-        private static void OnCommandPropertyChanged(DependencyObject d,
-            DependencyPropertyChangedEventArgs e)
-        {
-            var control = d as ListViewBase;
-            if (control != null)
-            {
-                control.ItemClick += OnItemClick;
-            }
+    //    private static void OnCommandPropertyChanged(DependencyObject d,
+    //        DependencyPropertyChangedEventArgs e)
+    //    {
+    //        var control = d as ListViewBase;
+    //        if (control != null)
+    //        {
+    //            control.ItemClick += OnItemClick;
+    //        }
 
-        }
+    //    }
 
-        private static void OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            var control = sender as ListViewBase;
-            var command = GetCommand(control);
+    //    private static void OnItemClick(object sender, ItemClickEventArgs e)
+    //    {
+    //        var control = sender as ListViewBase;
+    //        var command = GetCommand(control);
 
-            if (command != null && command.CanExecute(e.ClickedItem))
-            {
-                command.Execute(e.ClickedItem);
-            }
-        }
-    }
+    //        if (command != null && command.CanExecute(e.ClickedItem))
+    //        {
+    //            command.Execute(e.ClickedItem);
+    //        }
+    //    }
+    //}
 }
