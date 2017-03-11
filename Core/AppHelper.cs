62031310a70ffbe5b0a1178edc66f9823a1ac065
mfe <martin.fetzel@get-the-solution.net>
2017-03-11 13:30:46 +0100

    Added AppHelper

---
+++ Core/AppHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace Get.the.solution.UWP.XAML
+{
+    public static class AppHelper
+    {
+        public static String GetAppVersion()
+        {
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+            return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+        }
+    }
+}
