dc139b7b2b59063a00f2cc09dc0d1adfe87258df
Martin Fetzel <martin.fetzel@get-the-solution.net>
2018-04-07 14:44:46 +0200

    Added Converter for NavigationView

---
+++ Core/Converter/NavigationViewItemsConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace Get.the.solution.UWP.XAML.Converter
+{
+    public class NavigationViewItemsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value!=null && value as IEnumerable<MenuItem> !=null)
+            {
+                IEnumerable<MenuItem> menuItems = value as IEnumerable<MenuItem>;
+                List<NavigationViewItem> navigationViewItems = new List<NavigationViewItem>();
+                foreach (MenuItem item in menuItems)
+                {
+                    navigationViewItems.Add(MenuItemToNavigationViewItem(item));
+                }
+                return navigationViewItems;
+            }
+            else if(value != null && value as MenuItem != null)
+            {
+                MenuItem menuItem = value as MenuItem;
+                return MenuItemToNavigationViewItem(menuItem);
+            }
+            return value;
+        }
+        public MenuItem NavigationViewItemToMenuItem(NavigationViewItem navigationViewItem)
+        {
+            MenuItem menuItem = new MenuItem();
+            menuItem.Name = navigationViewItem.Content?.ToString();
+            menuItem.PageType = navigationViewItem.Tag as Type;
+            menuItem.Icon = (navigationViewItem.Icon as SymbolIcon).Symbol;
+            return menuItem;
+        }
+        public NavigationViewItem MenuItemToNavigationViewItem(MenuItem menuItem)
+        {
+            NavigationViewItem navigationViewItem = new NavigationViewItem();
+            navigationViewItem.Content = menuItem?.Name;
+            navigationViewItem.Tag = menuItem?.PageType;
+            navigationViewItem.Icon = new SymbolIcon() { Symbol = menuItem.Icon };
+            return navigationViewItem;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null && value as NavigationViewItem != null)
+            {
+                NavigationViewItem navigationViewItem = value as NavigationViewItem;
+                return NavigationViewItemToMenuItem(navigationViewItem);
+            }
+            return value;
+        }
+    }
+}
